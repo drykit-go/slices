52ff4b729e1e9d437518a6bf096264948c353aeb
Gregory Albouy <gregoryalbouy75@gmail.com>
2021-11-01 17:38:41 +0100

    Implement Apply

--- slicex.go
+++ slicex.go
@@ -1,21 +1,24 @@
 package slicex
 
 type (
-	// FilterFunc is a function that returns a boolean depending on v.
-	FilterFunc[T any] func(v T) bool
+	// FilterFunc is a function that returns a boolean value
+	// that depends on the current element.
+	FilterFunc[Elem any] func(Elem) bool
 
-	// MapFunc is a function that transforms v into a value of any type.
-	MapFunc[T, U any] func(v T) U
+	// MapFunc is a function that transforms the current element into
+	// a new value of any type.
+	MapFunc[Elem, NewElem any] func(Elem) NewElem
 
-	// ReduceFunc is a reducer function.
-	ReduceFunc[U, T any] func(U, T) U
+	// ReduceFunc is a reducer function: it returns an accumulated value
+	// that depends on the previously accumulated value and the current element.
+	ReduceFunc[Accumulator, Elem any] func(Accumulator, Elem) Accumulator
 )
 
 // Map applies f on each element of src and returns the resulting slice.
 // The output is guaranteed to be the same length as src.
 // src remains unaltered.
-func Map[T, U any](src []T, f MapFunc[T, U]) []U {
-	out := make([]U, len(src))
+func Map[Elem, NewElem any](src []Elem, f MapFunc[Elem, NewElem]) []NewElem {
+	out := make([]NewElem, len(src))
 	for i, v := range src {
 		out[i] = f(v)
 	}
@@ -48,4 +51,12 @@ func Reduce[Elem, Accumulator any](
 		out = f(out, v)
 	}
 	return out
+}
+
+// Apply iterates over src and calls f(currentIndex, currentElement)
+// each iteration until the end is reached.
+func Apply[Elem any](src []Elem, f func(i int, v Elem)) {
+	for i, v := range src {
+		f(i, v)
+	}
 }
