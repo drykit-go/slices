c241ad812d47237497d5168904ac9125553d19fd
Gregory Albouy <60700958+GregoryAlbouy@users.noreply.github.com>
2021-11-01 16:28:36 +0100

    feat: implement main functions (#1)
    * Implement Map
    
    * Write unit tests for Map
    
    * Implement Filter
    
    * Write unit tests for Filter
    
    * Implement Reduce
    
    * Write unit tests for Reduce

--- slicex.go
+++ slicex.go
@@ -1,1 +1,51 @@
 package slicex
+
+type (
+	// FilterFunc is a function that returns a boolean depending on v.
+	FilterFunc[T any] func(v T) bool
+
+	// MapFunc is a function that transforms v into a value of any type.
+	MapFunc[T, U any] func(v T) U
+
+	// ReduceFunc is a reducer function.
+	ReduceFunc[U, T any] func(U, T) U
+)
+
+// Map applies f on each element of src and returns the resulting slice.
+// The output is guaranteed to be the same length as src.
+// src remains unaltered.
+func Map[T, U any](src []T, f MapFunc[T, U]) []U {
+	out := make([]U, len(src))
+	for i, v := range src {
+		out[i] = f(v)
+	}
+	return out
+}
+
+// Filter filters out elements of src for which f(element) returns false
+// and returns the resulting slice.
+// The output length is inferior or equal to src's length.
+// src remains unaltered.
+func Filter[T any](src []T, f FilterFunc[T]) []T {
+	out := []T{}
+	for _, v := range src {
+		if f(v) {
+			out = append(out, v)
+		}
+	}
+	return out
+}
+
+// Reduce applies reducer f to src starting from ini and returns
+// the accumulated value.
+func Reduce[Elem, Accumulator any](
+	src []Elem,
+	f ReduceFunc[Accumulator, Elem],
+	ini Accumulator,
+) Accumulator {
+	out := ini
+	for _, v := range src {
+		out = f(out, v)
+	}
+	return out
+}
---
+++ slicex_test.go
@@ -0,0 +1,51 @@
+package slicex_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/drykit-go/slicex"
+)
+
+func TestMap(t *testing.T) {
+	ints := []int{0, 5, -1}
+	toString := func(n int) string { return fmt.Sprint(n) }
+	exp := []string{"0", "5", "-1"}
+	got := slicex.Map(ints, toString)
+	assertEqualSlices(t, got, exp)
+}
+
+func TestFilter(t *testing.T) {
+	ints := []int{0, 5, -1}
+	isEven := func(n int) bool { return n&1 == 0 }
+	exp := []int{0}
+	got := slicex.Filter(ints, isEven)
+	assertEqualSlices(t, got, exp)
+}
+
+func TestReduce(t *testing.T) {
+	type bill struct {
+		label  string
+		amount float64
+	}
+	bills := []bill{
+		{label: "a", amount: 3.14},
+		{label: "b", amount: 39},
+		{label: "c", amount: -.14},
+	}
+	calculateSum := func(sum float64, cur bill) float64 {
+		return sum + cur.amount
+	}
+	exp := 42.
+	if got := slicex.Reduce(bills, calculateSum, 0); got != exp {
+		t.Errorf("exp %v\ngot %v", exp, got)
+	}
+}
+
+func assertEqualSlices[T any](t *testing.T, a, b []T) {
+	t.Helper()
+	if !reflect.DeepEqual(a, b) {
+		t.Errorf("exp equal slices, got:\na == %v\nb == %v", a, b)
+	}
+}
