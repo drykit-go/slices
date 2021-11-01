439294732a81d92ed2cdc731a89f9d93a5d61386
Gregory Albouy <gregoryalbouy75@gmail.com>
2021-11-01 15:50:46 +0100

    Write unit tests for Map

---
+++ slicex_test.go
@@ -0,0 +1,24 @@
+package slicex_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/drykit-go/slicex"
+)
+
+func TestMap(t *testing.T) {
+	ints := []int{0, 5, -1}
+	toString := func(n int) string { return fmt.Sprint(n) }
+	exp := []string{"0", "5", "-1"}
+	got := slicex.Map(ints, toString)
+	assertEqualSlices(t, got, exp)
+}
+
+func assertEqualSlices[T any](t *testing.T, a, b []T) {
+	t.Helper()
+	if !reflect.DeepEqual(a, b) {
+		t.Errorf("exp equal slices, got:\na == %v\nb == %v", a, b)
+	}
+}
