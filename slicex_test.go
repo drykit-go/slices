edb00e4a49e5c7b04c57d555a023b29f8936142f
Gregory Albouy <gregoryalbouy75@gmail.com>
2021-11-01 15:59:58 +0100

    Write unit tests for Filter

--- slicex_test.go
+++ slicex_test.go
@@ -16,6 +16,14 @@ func TestMap(t *testing.T) {
 	assertEqualSlices(t, got, exp)
 }
 
+func TestFilter(t *testing.T) {
+	ints := []int{0, 5, -1}
+	isEven := func(n int) bool { return n&1 == 0 }
+	exp := []int{0}
+	got := slicex.Filter(ints, isEven)
+	assertEqualSlices(t, got, exp)
+}
+
 func assertEqualSlices[T any](t *testing.T, a, b []T) {
 	t.Helper()
 	if !reflect.DeepEqual(a, b) {
